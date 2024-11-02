1fa66a1ed41d2aaf2bd23314ed37e883844311e0
Tim Z Yu <zaiyangyu369@gmail.com>
2024-11-02 00:59:51 -0400

    Delete login-logout-Authentication-Spring boot directory

--- login-logout-Authentication-Spring boot/src/main/java/com/woromedia/api/task/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.woromedia.api.task.repository;
-
-import com.woromedia.api.task.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
