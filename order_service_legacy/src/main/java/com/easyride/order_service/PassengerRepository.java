db455e43ac0751089600524be82cefe588ab5650
Tim Zaiyang Yu <zaiyangyu369@gmail.com>
2024-11-16 17:28:23 -0500

    Delete order_service_legacy directory

--- order_service_legacy/src/main/java/com/easyride/order_service/PassengerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.easyride.order_service;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-//PassengerRepository 提供了对乘客实体的数据库操作方法。
-@Repository
-public interface PassengerRepository extends JpaRepository<Passenger, Long> {
-}
-
