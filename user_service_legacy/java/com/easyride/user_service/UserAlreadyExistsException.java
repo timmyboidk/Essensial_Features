33da12144c40e3dab77e5f22ec771dfc3e8e5fe0
Tim Zaiyang Yu <zaiyangyu369@gmail.com>
2024-11-16 17:33:00 -0500

    Delete user_service_legacy directory

--- user_service_legacy/java/com/easyride/user_service/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.easyride.user_service;
-
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
-
